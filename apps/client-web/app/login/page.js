428dc1b1d1a004c23f031954e954dab3ab1e728c
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-19 23:34:19 -0600

    Create page.js

---
+++ apps/client-web/app/login/page.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import API from "../../lib/api";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await API.post("/auth/login", { email, password });
+    localStorage.setItem("token", res.data.token);
+    alert("Login exitoso");
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Login</h2>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Entrar</button>
+    </div>
+  );
+}
