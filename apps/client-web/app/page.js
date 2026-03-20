80b088cfcd1c6429e5b4d569d0f48c4e804cd732
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-19 23:38:13 -0600

    Update page.js

--- apps/client-web/app/page.js
+++ apps/client-web/app/page.js
@@ -9,26 +9,17 @@ export default function Home() {
 
   useEffect(() => {
     API.get("/restaurants")
-      .then(res => setRestaurants(res.data))
-      .catch(err => console.error(err));
+      .then(res => setRestaurants(res.data));
   }, []);
 
   return (
-    <div style={{ padding: 20 }}>
+    <div className="container">
       <h2>Hola 👋</h2>
       <h3>¿Qué te gustaría pedir hoy?</h3>
 
-      <input
-        placeholder="Buscar..."
-        style={{
-          width: "100%",
-          padding: 10,
-          borderRadius: 10,
-          border: "1px solid #ccc"
-        }}
-      />
+      <input className="input" placeholder="Buscar comida..." />
 
-      <h3 style={{ marginTop: 20 }}>Restaurantes</h3>
+      <h3>Restaurantes Cercanos</h3>
 
       {restaurants.map(r => (
         <RestaurantCard key={r._id} restaurant={r} />
