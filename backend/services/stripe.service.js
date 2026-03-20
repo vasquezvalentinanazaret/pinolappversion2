da7cbc093ca2d0750819bb642f436c7431f1e115
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-20 00:02:27 -0600

    Update stripe.service.js

--- backend/services/stripe.service.js
+++ backend/services/stripe.service.js
@@ -2,9 +2,23 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET);
 
-export const createPaymentIntent = async (amount) => {
-  return await stripe.paymentIntents.create({
-    amount: amount * 100, // centavos
-    currency: "usd"
+export const createCheckoutSession = async (amount) => {
+  return await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "Pedido PinolApp"
+          },
+          unit_amount: amount * 100
+        },
+        quantity: 1
+      }
+    ],
+    mode: "payment",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/cancel"
   });
 };
