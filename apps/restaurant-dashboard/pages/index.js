e914eb03c7536b00b6b49a8f110ebb6dabff6eac
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-19 23:52:18 -0600

    Create index.js

---
+++ apps/restaurant-dashboard/pages/index.js
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Panel Restaurante 🍔</h1>
+
+      <a href="/orders">Ver Pedidos</a>
+      <br />
+      <a href="/foods">Gestionar Menú</a>
+    </div>
+  );
+}
