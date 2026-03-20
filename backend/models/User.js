9cb63bafc11fc4076a00dd701deea657aa931c9b
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-19 23:25:06 -0600

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: {
+    type: String,
+    enum: ["user", "driver", "restaurant", "admin"],
+    default: "user"
+  }
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
