82943388125d414e2d1fde00080bce52dc8adabf
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-19 23:49:06 -0600

    Create App.js

---
+++ apps/driver-app/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+
+export default function App() {
+  return (
+    <View style={{ marginTop: 40 }}>
+      <HomeScreen />
+    </View>
+  );
+}
