88853a72c5f0628c1814a852dc451afd8ab8a552
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-19 23:45:13 -0600

    Update order.controller.js

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,11 +1,15 @@
 import Order from "../models/Order.js";
 
 export const createOrder = async (req, res) => {
-  const order = await Order.create(req.body);
+  const order = await Order.create({
+    ...req.body,
+    userId: req.user.id
+  });
+
   res.json(order);
 };
 
 export const getOrders = async (req, res) => {
-  const orders = await Order.find();
+  const orders = await Order.find({ userId: req.user.id });
   res.json(orders);
 };
