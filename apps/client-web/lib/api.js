d5b14c91202c51c13cb785ec70996e8612c01bcc
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-20 00:06:03 -0600

    Update api.js

--- apps/client-web/lib/api.js
+++ apps/client-web/lib/api.js
@@ -1,17 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
-});
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+  baseURL: "https://TU-BACKEND.onrender.com/api"
 });
 
 export default API;
